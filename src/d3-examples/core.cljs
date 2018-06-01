8671c3c59005b2153f5e83ad0ee008317a009e86
jay doubleu <jay@jayeldoubleu.com>
2018-06-01 16:26:18 -0500

    Implementing examples 47-57
    Introduction to d3 concepts: Scale, Domain, Range.  One example
    with dom manipulation adding an svg and rect element

---
+++ src/d3-examples/core.cljs
@@ -0,0 +1,88 @@
+(ns min-figwheel.core
+  (:require [cljsjs/d3]))
+
+; Example 47
+; Print out x and y coordinates. Notice they are strings.
+(defn prn-concat
+  [data]
+  (doseq [{:strs [x y]} (js->clj data)]
+    (prn x y)))
+
+(.csv js/d3 "data.csv" prn-concat)
+
+; Example 48
+; Print out sum of x and y coordinates. Use type function to process datum.
+(defn type-func
+  [datum]
+  (-> (js->clj datum)
+      (update "x" js/parseFloat)
+      (update "y" js/parseFloat)
+      (clj->js)))
+
+(defn prn-addition
+  [data]
+  (doseq [{:strs [x y]} (js->clj data)]
+    (prn (+ x y))))
+
+(.csv js/d3 "data.csv" type-func prn-addition)
+
+; Example 49
+; Skipped +datum.x syntax not applicable
+
+; Example 50
+; Example introducing scale, domain, and range
+(def scale-50 (-> js/d3
+                  (.scaleLinear)
+                  (.domain #js [0 1])
+                  (.range #js [0 100])))
+(prn "Ex 50 " (scale-50 0))
+(prn "Ex 50 " (scale-50 0.5))
+(prn "Ex 50 " (scale-50 1))
+
+; Example 51
+; Skipped already covered with threading macro above
+
+; Example 52
+; I guess showing that .domain and .range are both setters and getters
+(prn "Ex 52 " (.domain scale-50))
+(prn "Ex 52 " (.range scale-50))
+
+; Example 53
+; Example of ordinal scale
+(def domain-letters ["A" "B" "C" "D"])
+(def range-fruits ["Apples" "Bananas" "Coconuts" "Durian"])
+(def scale-53 (-> js/d3
+                  (.scaleOrdinal)
+                  (.domain (clj->js domain-letters))
+                  (.range (clj->js range-fruits))))
+(prn "Ex 53 " (scale-53 "A"))
+(prn "Ex 53 " (scale-53 "B"))
+(prn "Ex 53 " (scale-53 "C"))
+
+; Example 54
+; Example of ordinals to range
+(def scale-54 (-> js/d3
+                  (.scalePoint)
+                  (.domain (clj->js domain-letters))
+                  (.range #js [0 100])))
+(prn "Ex 54 " (scale-54 "A"))
+(prn "Ex 54 " (scale-54 "B"))
+(prn "Ex 54 " (scale-54 "C"))
+
+; Example 55
+; Uses rounding
+(def scale-55 (.round scale-54 true))
+(prn "Ex 55 " (scale-55 "A"))
+(prn "Ex 55 " (scale-55 "B"))
+(prn "Ex 55 " (scale-55 "C"))
+
+; Example 56
+; First dom manipulation. Ensure that div is above script in html. 
+(-> js/d3
+    (.select "div#ex-56")
+    (.append "svg")
+    (.append "rect")
+    (.attr "x" 50)
+    (.attr "y" 50)
+    (.attr "width" 20)
+    (.attr "height" 20))
